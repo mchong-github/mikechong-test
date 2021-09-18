2ad3a3b82ae3300f34fee1c425911d51d96461db
ramenz <orious@zoho.com>
2021-09-18 16:08:16 -0400

    20210917-0335

---
+++ scripts/stop-job-in-dev.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nomad job stop -address='http://127.0.0.1:4646' -purge run-apps
+
--- scripts/stop-nomad-dev.sh
+++ scripts/stop-nomad-dev.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-nomad job stop -address='http://127.0.0.1:4646' -purge run-apps
-sleep 5
 pkill nomad
